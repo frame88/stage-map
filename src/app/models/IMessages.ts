0e9c2aaf8f1c915fd63326d4d393a4c4c29c7db8
Francesco <francesco.leuzzi@intellitronika.com>
2022-12-02 17:18:02 +0100

    login from previously repo: "cartList-work"

---
+++ src/app/models/IMessages.ts
@@ -0,0 +1,6 @@
+export interface IMessage{
+  sender: string;
+  receiver: string;
+  timestamp: Date;
+  text: string;
+}
---
+++ src/app/nuovapagina/nuovapagina.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuovapagina',
+  templateUrl: './nuovapagina.page.html',
+  styleUrls: ['./nuovapagina.page.scss'],
+})
+export class NuovapaginaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
